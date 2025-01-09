69438c3f9932dbff578daa0e762e2c4dd0eae5f9
cadenrizan <cadenrizan123@gmail.com>
2025-01-09 19:45:25 +0000

    added databases

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -48,7 +48,7 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
+    createCollectable("steve", 800, 300, 20, 0.5);
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
